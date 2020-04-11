498db66c48756db8d718e81fde8aced0e7b8bbb5
Herzen Cortes <herzencc@icloud.com>
2020-04-10 22:52:31 -0400

    first project, first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(`hello`)
+
+
+// adding button function to answers
+
+document.querySelector(".btn1").addEventListener("click", (e) => console.log(e.target))
+document.querySelector(".btn2").addEventListener("click", (e) => console.log(e.target))
+document.querySelector(".btn3").addEventListener("click", (e) => console.log(e.target))
