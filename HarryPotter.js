a248adad35284db8ad6b40a511c728cbce3117ad
Herzen Cortes <herzencc@icloud.com>
2020-04-14 18:10:43 -0400

    changes in logic

---
+++ HarryPotter.js
@@ -0,0 +1,222 @@
+var harryPotterArr = [
+    {
+        q: "What happens when Hagrid casts a spell on Dudley?",
+        a: "He grows a tail",
+        btn1: "He grows pig ears",
+        btn2: "He grows a pig snout",
+        btn3: "He grows pig ears"
+  
+    },
+    {
+        q: "Which Spell can unlock doors",
+        a: "Alohomora",
+        btn1: "Alohomora",
+        btn2: "Asahomora",
+        btn3: "Wingardium Leviosa"
+      
+    },
+    {
+        q: "Which spell can levitate objects?",
+        a: "Wingardium Leviosa",
+        btn1: "Leviticus Totalus",
+        btn2: "Expeliamus",
+        btn3: "Wingardium Leviosa"
+      
+    },
+    {
+        q: "On which object does Ron cast Wingardium Leviosa when in the girls’ bathroom?",
+        a: "The Troll's club",
+        btn1: "A feather",
+        btn2: "A tree",
+        btn3: "The troll's club"
+    },
+    {
+        q: "Which spell is also known as the full Body-Bind? ",
+        a: "Petrificus Totalus",
+        btn1: "Locomotor Mortis",
+        btn2: "Cogeladus Totalus",
+        btn3: "Petrificus Totalus"
+    },
+    {
+        q: "Which spell is also known as the Leg-Locker Curse",
+        a: "Locomotor Mortis",
+        btn1: "Locomotor Mortis",
+        btn2: "Petrifucs Totalus",
+        btn3: "Expeliamus"
+    },
+    {
+        q: "From which platform does the Hogwarts Express depart? ",
+        a: "9 3/4",
+        btn1: "9",
+        btn2: "9 1/4",
+        btn3: "9 3/4"
+    },
+    {
+        q: "Where does Hagrid tale Harry to buy School Supplies?",
+        a: "Diagon Alley",
+        btn1: "",
+        btn2: "",
+        btn3: ""
+    },
+    {
+        q: "Where do wizards go to deposit money?",
+        a: "Gringots",
+        btn1: "Diagon Alley",
+        btn2: "Diagonally",
+        btn3: "The Leaky Cauldron"
+    },
+    {
+        q: "What is the number of the vault where Hagrid picks up the Sorcerer's Stone?",
+        a: "713",
+        btn1: "9 3/4",
+        btn2: "142",
+        btn3: "713"
+    },
+    {
+        q: "Who is a muggle?",
+        a:"A person that can't do magic",
+        btn1: "A person that can't do magic",
+        btn2: "A person with one magical parent and a non-magical parent",
+        btn3: "A person with non-magical parents"
+    },
+    {
+        q: "What is the name of Harry's Owl?",
+        a: "Hedwig",
+        btn1: "Hagrid",
+        btn2: "Hedwig",
+        btn3: "Scabbers"
+    },
+    {
+        q: "What is the name of the room Harry uses to teach Dumbledore's Army?",
+        a:"The room of requirement",
+        btn1: "The room in need",
+        btn2: "The Chamber of Secrets",
+        btn3: "The rom of requirement"
+    },
+    {
+        q: "Who is the Hufflepuff house ghost",
+        a: "The Fat Friar",
+        btn1: "Moping Myrtle",
+        btn2: "Nearly Headless Nick",
+        btn3: "The Fat Friar"
+    },
+    {
+        q:"How many staircases does Hogwarts have?",
+        a:"142",
+        btn1: "146",
+        btn2: "172",
+        btn3: "142"
+    },
+    {
+        q:"Where is the Slytherin common room located?",
+        a:"The Dungeons",
+        btn1: "The Dungeons",
+        btn2: "Under the kitchen",
+        btn3: "In the west tower"
+    },
+    {
+        q:"Who was the headmaster of Hogwarts when the Chamber of Secrets was first opened?",
+        a:"Armando Dippet",
+        btn1: "Armando Dippet",
+        btn2: "Albus Dumbledore",
+        btn3: "Tom Riddle"
+    },
+    {
+        q:"What is the name of the book Hermione supposes Voldemort used to learn about Horcruxes?",
+        a:"Secrets of the Darkest Arts",
+        btn1: "Powers of the Darkest Arts",
+        btn2: "Secrets of the Darkest arts",
+        btn3: "Potions of the Darkest Arts"
+    },
+    {
+        q:"Which Hogwarts Professor was rumored to be a duelling champion in their youh?",
+        a:"Filius Flitwick",
+        btn1: "Severus Snape",
+        btn2: "Albus Dumbledore",
+        btn3: "Filius Flitwick"
+    },
+    {
+        q:"Which creatures pull the carriages that take students from the Hogwarts Express to the Castle?",
+        a:"Thestrials",
+        btn1: "Ponies",
+        btn2: "Thestrials",
+        btn3: "Hippogrif"
+    },
+    {
+        q:"Slughorn teaches his students that Amortentia smells different to each person. WHat food does Harry Smell?",
+        a:"Treacle Tart",
+        btn1: "Lemon Drops",
+        btn2: "Pumpkin Juice",
+        btn3: "Treacle Tart"
+    },
+    {
+        q:"What id Gryffindor's house ghost?",
+        a:"Sir Nicholas de Mimsy Porpington",
+        btn1: "Moping Myrtle",
+        btn2: "Sir Nicholas de Mimsy Porpington",
+        btn3: "The Grey Lady"
+    },
+    {
+        q:"Harry, Ron, and Hermione help save the Sorcerer's Stone from being stolen. How old was its co-creator, Nicholas Flamel, when he decided to destroy it?",
+        a:"665",
+        btn1: "745",
+        btn2: "665",
+        btn3: "198"
+    },
+    {
+        q:"Monkshood and wolfsbane are the same plant, also known as what?",
+        a:"Acernine",
+        btn1: "Mandrake",
+        btn2: "Gilly Weed",
+        btn3: "Acernine"
+    },
+    {
+        q:"How many possible Quidditch fouls are there?",
+        a:"700",
+        btn1: "789",
+        btn2: "709",
+        btn3: "700"
+    },
+    {
+        q:"Dumbledore has a scar above his left knee that is a perfect map of what?",
+        a:"The London Under Ground",
+        btn1: "The Longon Under Ground",
+        btn2: "London",
+        btn3: "The Dark Forest"
+    },
+    {
+        q: "Where does Vernon Dursley work, and what does the company produce?",
+        a: "Grunnings, a drill manufacturer",
+        btn1: "Cummings, diesel engines",
+        btn2: "Grunnings, a drill manufacturer",
+        btn3: "Longings, Hammers"
+    },
+    {
+        q:"Cedric Diggory let Harry use the prefect's bathroom in \"The Goblet of Fire.\" What man's statue is next to the special lavatory entrance?",
+        a:"Boris the Bewildered",
+        btn1: "Tom Riddle",
+        btn2: "Albus Dumbledore",
+        btn3: "Boris the Bewildered"
+    },
+    {
+        q:"What is the max speed for a Firebolt Broomstick",
+        a:"150 mph",
+        btn1: "150 mph",
+        btn2: "175 mph",
+        btn3: "120 mph"
+    },
+    {
+        q:"Which row contains the prophecy about Harry and Voldemort?",
+        a:"97",
+        btn1: "90",
+        btn2: "97",
+        btn3: "89"
+    },
+    {
+        q:"On the wall across from the Room of Requirement, there is a tapestry showing a wizard trying to teach trolls ballet. What's his name?",
+        a:"Barnabus the Barmy",
+        btn1: "Bernardo the Savage",
+        btn2: "Barnabus the Barmy",
+        btn3: "Wilfred the Wise"
+    }
+  ]
--- index.js
+++ index.js
@@ -1,306 +1,43 @@
-//DOM
-
-/* To set the right answe I will create an array containing all of the right answers and using the
-ternary function above I will check the content of the buttons against this array, this will add the 
-class .btn right. 
-I need to create an array of all the questions and 3 arrays with possible answers. add an event listener to 
-the class btn-right and on click will change all of the options*/
-
-// question and variable arrays
-
-var questionArr = [
-  `What is the name of Han Solo’s ship?`, 
-  `What is the weapon used by Jedi Knights?`,
-  `Which young Jedi Knight becomes Darth Vader in Star Wars: Episode III Revenge of the Sith?`,
-  `What Jedi Master spent 800 years training such pupils as Qui-Gon Jinn Obi-Wan Kenobi, Count Dooku and Luke Skywalker?`,
-  `What Wookiee is Han Solo’s loyal friend and first mate?`,
-  `Who is Luke and Leia’s mother?`,
-  `How many members are there in the Jedi Council?`,
-  `Why did Yoda say he didn’t want to train Luke Skywalker?`,
-  `In the Star Wars film, what invisible power binds the galaxy together?`,
-  `Who are the masters of the Dark Side who want to rule the galaxy?`,
-  `What were the last words spoken in The Empire Strikes Back?`,
-  `What animals did the visual effects crew study when designing the Imperial Walkers from The Empire Strikes Back?`,
-  `Which country was the home base for principal photography on Revenge of the Sith?`,
-  `Which Star Wars movie was the first to be shot entirely with high-definition cameras, without using traditional 35-millimeter film?`,
-  `What U.S. magazine declared Star Wars “the year’s best movie” in its May 30, 1977, issue?`,
-  `Which star of Revenge of the Sith is the real-life nephew of Denis Lawson, who played Wedge in the first Star Wars Trilogy?`,
-  `According to Star Wars legend, what substance powers lightsabers?`,
-  `How much does the Chewbacca costume weigh?`,
-  `Yoda did not make an appearance in only one of the six Star Wars movies. Which one?`,
-  `What famous scientist was the inspiration for creature designer Stuart Freeborn when creating Yoda’s eyes?`,
-  `Padmé Amidala is first queen of, and then the senator from, what peaceful planet?`,
-  `What Ewok medicine man presided over the attempt to make a roast of Han Solo in Return of the Jedi?`,
-  `Lando Clarissian’s operation on Cloud City mines what type of gas?`,
-  `The Empire attacked the Rebel base on Hoth with four-legged Imperial Walkers, or AT-ATs. What does AT-AT stand for?`,
-  `What planet is home to Chewbacca and the Wookiees?`,
-  `What is the species of the diminutive workers on Cloud City who helped freeze Han Solo in carbonite?`,
-  `What type of battle armor does Boba Fett wear?`,
-  `Han Solo upgraded his beloved Millennium Falcon from which stock starship?`,
-  `What color is Yoda’s lightsaber in Attack of the Clones and Revenge of the Sith?`,
-  `Complete this line from A New Hope: “The ____________ Wastes are not to be traveled lightly."`,
-  `Who preceded Senator Palpatine as Chancellor of the Galactic Senate?`,
-  `How many engines are on an X-wing fighter?`,
-  `Bail Organa is Princess Leia’s adoptive father. What is her adoptive mother’s name?`,
-]
-
-var rightAnswers = [
-  `Millennium Falcon`,
-  `Lightsaber`,
-  `Anakin Skywalker`,
-  `Yoda`,
-  `Chewie`,
-  `Padmé Amidala`,
-  `12`,
-  `because he lacked patience`,
-  `The Force`,
-  `The Sith`,
-  `“May the Force be with you.”`,
-  `Elephants`,
-  `Australia`,
-  `Attack of the Clones`,
-  `Time`,
-  `Ewan McGregor`,
-  `Ilum crystals`,
-  `8 pounds`,
-  `Star Wars: Episode IV A New Hope`,
-  `Albert Einstein`,
-  `Naboo`,
-  `Logray`,
-  `Tibanna gas`,
-  `All Terrain-Armored Transport`,
-  `Kashyyyk`,
-  `Ugnaughts`,
-  `Mandalorian`,
-  `The YT-1300 stock light freighter`,
-  `Green`,
-  `Jundland`,
-  `Chancellor Valorum`,
-  `4`,
-  `Queen Breha Organa`
-]
-
-let btn1 = [
-`Millennium Falcon`,
-`Lightsaber`,
-`Anakin Skywalker`,
-`Yoda`,
-`Chewie`,
-`Padmé Amidala`,
-`12`,
-`because he lacked patience`,
-`The Force`,
-`The Sith`,
-`"Luke I am your Father"`,
-`Hippos`,
-`Ethiopia`,
-`Return of the Jedi`,
-`Forbes`,
-`Harrison Ford`,
-`Ice Crystals`,
-`6 pounds`,
-`Star Wars: Episode V The Empire Strikes Back`,
-`Isaac Newton`,
-`Tatooine`,
-`Jabush`,
-`tintin gas`,
-`All Terrain All Attack`,
-`Kashan`,
-`Sand People`,
-`Jedi`,
-`L3's memory drive`,
-`Purple`,
-`Naboo`,
-`Chancellor Organa`,
-`2`,
-`Padme`,
-// answer options for button `
-]
-
-let btn2 = [
-  // enter wrong answers for indexes 1-9
-  `Silver Bullet`,
-  `Shooters`,
-  `Luke Skywalker`,
-  `Qui Gon Jinn`,
-  `Padme Amidala`,
-  `Princess Leia`,
-  `13`,
-  `Because he had no talent`,
-  `The Strength`,
-  `The Senate`,
-  `“May the Force be with you.”`,
-  `Elephants`,
-  `Australia`,
-  `Attack of the Clones`,
-  `Time`,
-  `Ewan McGregor`,
-  `Ilum crystals`,
-  `8 pounds`,
-  `Star Wars: Episode IV A New Hope`,
-  `Stephen Hawking`,
-  'Organa',
-  'Largaa',
-  `Muhana Gas`,
-  `All Attack Acessible Transport`,
-  `Mandalore`,
-  `Garnaughts`,
-  `Gungan`,
-  `The Death Star`,
-  `yellow`,
-  `Tatooine`,
-  `Chancellor Obi Wan Kenobi`,
-  `5`,
-  `Queen Jamilia`,
-  // Answer options for second button 
-]
-
-let btn3 = [
- // write in answers for all indexes before 18 and before 
- `J Type 327 Nubian`,
- `Blasters`,
- `Ben Solo`,
- `Yada`,
- `Jamilia Amidala`,
- `Ahsoka Tano`,
- `14`,
- `Because he had no dedication`,
- `The magic`,
- `The Council`,
- `“Do or do not, there is no try”`,
- `Giraffe`,
- `Tunisia`,
- `Attack of the Droids`,
- `Rolling Stone`,
- `Liam Neeson `,
- `Karma crystals`,
- `9 pounds`,
- `Star Wars: Episode III Revenge of the Sith`,
-  `Albert Einstein`,
-  `Naboo`,
-  `Logray`,
-  `Tibanna gas`,
-  `All Terrain-Armored Transport`,
-  `Kashyyyk`,
-  `Ugnaughts`,
-  `Mandalorian`,
-  `The YT-1300 stock light freighter`,
-  `Green`,
-  `Jundland`,
-  `Chancellor Valorum`,
-  `4`,
-  `Queen Breha Organa`
-]
-
-
-
-// var questionIndex = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]
-
-
-
-// function shuffle(array) {
-//   array.sort(() => Math.random() - 0.5);
-//   console.log(array)
-//   return array
-// }
-
-// let random = shuffle(questionIndex)
-
-// Conditional statements establishing right question
-// rightAnswers.includes(document.querySelector(`.btn1`).value) ? document.querySelector(`.btn1`).classList.add("btn-right") : document.querySelector(`.btn1`).classList.add("btn-wrong")
-// rightAnswers.includes(document.querySelector(`.btn2`).value) ? document.querySelector(`.btn2`).classList.add("btn-right") : document.querySelector(`.btn2`).classList.add("btn-wrong")
-// rightAnswers.includes(document.querySelector(`.btn3`).value) ? document.querySelector(`.btn3`).classList.add("btn-right") : document.querySelector(`.btn3`).classList.add("btn-wrong")
-
-// event listeners for buttons 
-//  document.querySelector(".btn1").addEventListener("click", function(e){ // first button
-//      console.log(e.target)
-//  })
-
-//  document.querySelector(".btn2").addEventListener("click", function(e){ // second button 
-//     console.log(e.target)
-// })
-
-// document.querySelector(".btn3").addEventListener("click", function(e){ // third button 
-//     console.log(e.target)
-// })
-
-// document.querySelector(".start-btn").addEventListener("click", function(e){ // start-button  
-//     console.log(e.target)
-// })
-
-
-
-
-
 // random question generator
 
-var questionIndex = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32]
+var game = starWarsArr
+var index = 0
 
 
-
-function shuffle(array) {
-array.sort(() => Math.random() - 0.5);
-return array
-}
-let random = shuffle(questionIndex)
-
-
-const randomQuestion = () => {
-console.log(random)
-for (let i = 0; i < questionIndex.length; i++) {
-//question
-  document.querySelector("#question").innerHTML = questionArr[random[i]] // selects a new question
-  //btn 1
-  document.querySelector(".btn1").value = btn1[random[i]] // sets the option for the button
-  document.querySelector(".btn1").classList.remove("btn-wrong","show","btn-right") // removes the class
-  // console.log(document.querySelector(".btn1").className)
-  // rightAnswers.includes(document.querySelector(`.btn1`).value) ? document.querySelector(`.btn1`).classList.add("btn-right") : document.querySelector(`.btn1`).classList.add("btn-wrong") // sets the new class
- //btn 2
-  document.querySelector(".btn2").value = btn2[random[i]]
-  document.querySelector(".btn2").classList.remove("btn-wrong","show","btn-right")
-  // console.log(document.querySelector(".btn2").className)
-  // rightAnswers.includes(document.querySelector(`.btn2`).value) ? document.querySelector(`.btn2`).classList.add("btn-right") : document.querySelector(`.btn2`).classList.add("btn-wrong") // do after event listener, dont add classes ahead of time 
-  // btn 3
-  document.querySelector(".btn3").value = btn3[random[i]]
-  document.querySelector(".btn3").classList.remove("btn-wrong","show","btn-right")
-  // console.log(document.querySelector(".btn3").className)
-  // rightAnswers.includes(document.querySelector(`.btn3`).value) ? document.querySelector(`.btn3`).classList.add("btn-right") : document.querySelector(`.btn3`).classList.add("btn-wrong")
-}
+const shuffle = (arr) => {
+arr.sort(() => Math.random() - 0.5);
+return arr
 }
 
-randomQuestion()
+let random = shuffle(game)
 
-const questionChange = () => {
-document.querySelector(".btn1").addEventListener("click", function(e) { 
-  // document.querySelector(".btn1").classList.contains("btn-wrong") ? document.querySelector(".btn1").style.color='red' : null
-  rightAnswers.includes(document.querySelector(`.btn1`).value) ? document.querySelector(".btn1").classList.add("btn-right") : document.querySelector(".btn1").classList.add("btn-wrong") // sets the new class
-  console.log(e.target)
-  document.querySelector(".btn1").classList.add("show")
-  document.querySelector(".btn1").classList.contains("btn-right")? (milleniumFalcon.y -= 55, randomQuestion()) : null
-})
-document.querySelector(".btn2").addEventListener("click", function(e) { 
-  // document.querySelector(".btn2").classList.contains("btn-wrong") ? document.querySelector(".btn2").style.color='red' : null
-  rightAnswers.includes(document.querySelector(`.btn2`).value) ? document.querySelector(".btn2").classList.add("btn-right") : document.querySelector(".btn2").classList.add("btn-wrong") // do after event listener, dont add classes ahead of time 
-  document.querySelector(".btn2").classList.add("show")
-  document.querySelector(".btn2").classList.contains("btn-right")? (milleniumFalcon.y -= 55, randomQuestion()) : null
+const question = () => {
+document.querySelector("#question").innerHTML = random[index].q
+document.querySelector(".btn1").value = random[index].btn1
+document.querySelector(".btn2").value = random[index].btn2
+document.querySelector(".btn3").value = random[index].btn3
 
-})
-
-document.querySelector(".btn3").addEventListener("click", function(e) { 
-  // document.querySelector(".btn3").classList.contains("btn-wrong") ?  document.querySelector(".btn2").style.color='red' : null
-  rightAnswers.includes(document.querySelector(`.btn3`).value) ? document.querySelector(".btn3").classList.add("btn-right") : document.querySelector(".btn3").classList.add("btn-wrong")
-  document.querySelector(".btn3").classList.add("show")
-  document.querySelector(".btn3").classList.contains("btn-right")? (milleniumFalcon.y -= 55, randomQuestion()) : null
-
-})
-
-// document.querySelector(".btn-right").addEventListener("click", function(e){ // third button 
-// milleniumFalcon.y -= 55
-// randomQuestion()
-// })
 }
 
-questionChange()
+const rightAnswer = () => {
+  answer=random[index].a
+  return answer
+}
+
+let answer = question()
+rightAnswer()
+
+document.querySelector(".btn1").onclick = function(e) {
+  document.querySelector(".btn1").value === answer ? (index++, question(), rightAnswer()) : console.log('wrong')
+}
+
+document.querySelector(".btn2").onclick = function(e) {
+  document.querySelector(".btn2").value === answer ? (index++, question(), rightAnswer()) : console.log('wrong')
+}
+
+document.querySelector(".btn3").onclick = function(e) {
+  document.querySelector(".btn3").value === answer ? (index++, question(), rightAnswer()) : console.log('wrong')
+}
 
 document.querySelector("#instructions > input").addEventListener("click", function(e){
 startGame()
@@ -315,6 +52,7 @@ startGame()
   
   var i = 500
   var j = 500
+
 
   let millFalconImg = new Image ()
   millFalconImg.src = './Images/Millennium-Falcon-Star-Wars-PNG-Image.png'
@@ -331,6 +69,8 @@ const drawMillFalcon = () => {
     y: i,
     image: millFalconImg  // makes an object of the road image, we can refer to this later 
   }
+
+
 
   let starDestroyerImg = new Image ()
   starDestroyerImg.src = './Images/Star-Destroyer-PNG-Photo-Image.png'
@@ -444,10 +184,3 @@ if(playerScore === 1) {
 
   animate()
   };
-
-
-
-
-
-
-
