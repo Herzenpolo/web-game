ec70c7085d3cbb910d659dc6ef8709759e73a80a
Herzen Cortes <herzencc@icloud.com>
2020-04-16 23:06:31 -0400

    beer trivia begin

---
+++ beer.js
@@ -0,0 +1,226 @@
+var beerArr = [
+    {
+        q:"The four main ingredients in beer are:",
+        a:"Water, hops, yeast and malts",
+        btn1:"Water, hops, yeast and malts",
+        btn2:"Malt, water, sugar, hops",
+        btn3:"Sugar, yeast, wheat, water"
+    },
+    {
+        q:"Wort is:",
+        a:"The liquidy leftovers from steeping and then straining crushed brew grains in hot water",
+        btn1:"A batch of beer that goes bad during the brewing process",
+        btn2:"The liquidy leftovers from steeping and then straining crushed brew grains in hot water",
+        btn3:"Something I don’t even want to associate with beer, thank you very much!"
+    },
+    {
+        q:"Which is not a style of beer:",
+        a:"Brubier",
+        btn1:" Roggenbier",
+        btn2:"Grodziskie",
+        btn3:"Brubier"
+    },
+    {
+        q:"What does IBU mean?",
+        a:"The scale that measures a beer’s bitterness",
+        btn1:"The scale that measures a beer’s bitterness",
+        btn2:"Another term for ABV",
+        btn3:"A hop-laden style of beer"
+    },
+    {
+        q:"Which statement is accurate:",
+        a:"Ales ferment at warmer temperatures with yeast on top whereas lagers ferment cooler and from the bottom.",
+        btn1:"Lagers ferment at warmer temperatures with yeast on top, where as ales ferment cooler and from the bottom.",
+        btn2:"Ales ferment at warmer temperatures with yeast on top whereas lagers ferment cooler and from the bottom.",
+        btn3:"Both ales and lagers ferment at the same temperature from the bottom."
+    },
+    {
+        q:"The oldest drinkable beer was found in a ship wreck and was _____ years ol",
+        a:"200 years old",
+        btn1:"10 years old",
+        btn2:"100 years old",
+        btn3:"200 years old"
+    },
+    {
+        q:"How many different cersions of Guinness are there?",
+        a:"19",
+        btn1:"17",
+        btn2:"18",
+        btn3:"19"
+    },
+    {
+        q:"what does Cenosillicaphobia mean?",
+        a:"Fear of an empty glass",
+        btn1:"Fear of an empty glass",
+        btn2:"Fear of a flat beer",
+        btn3:"Fear of a warm beer"
+    },
+    {
+        q:"Which country is the top hops producer in the world?",
+        a:"Germany",
+        btn1:"United States",
+        btn2:"Germany",
+        btn3:"Hungary"
+    },
+    {
+        q:"What country produces the second most hops in the world?",
+        a:"United States",
+        btn1:"Germany",
+        btn2:"United States",
+        btn3:"Mexico"
+    },
+    {
+        q:"Frosty glasses will...",
+        a:"Cause your beer to foam",
+        btn1:"Cause your beer to stay carbonated",
+        btn2:"Cause your beer to foam",
+        btn3:"Cause your beer to lose flavor"
+    },
+    {
+        q:"Why were barrel-aged beers created?",
+        a:"By mistake!",
+        btn1:"By mistake!",
+        btn2:"It sounded like a good idea to ferment beer in whiskey",
+        btn3:"A distillery wanted to market to a younger demographic"
+    },
+    {
+        q:"How many medicinal uses for beer were documented by egyptians?",
+        a:"100+",
+        btn1:"100+",
+        btn2:"50-75",
+        btn3:"1-50"
+    },
+    {
+        q:"How many liters of beer did the average person drink in medieval europe?",
+        a:"220-250",
+        btn1:"100-150",
+        btn2:"220-250",
+        btn3:"less than 100"
+    },
+    {
+        q:"Why did people drink beer in the middle ages?",
+        a:"It was cleaner and safer to drink than water.",
+        btn1:"Because it quenches thirst",
+        btn2:"Because beer is delicious and fresh",
+        btn3:"It was cleaner and safer to drink than water."
+    },
+    {
+        q:"What is the reason for the Mayflower landing at Plymouth Rock? (according to a diary entry)",
+        a:"due to a lack of beer",
+        btn1:"due to a lack of beer",
+        btn2:"due to a lack of whiskey",
+        btn3:"due to a lack of food"
+    },
+    {
+        q:"What was Viking heaven?",
+        a:"An awaiting giant goat with an undending supply of beer coming her utters",
+        btn1:"An awaiting giant goat with an undending supply of beer coming her utters",
+        btn2:"A giant pool of beer",
+        btn3:"A boat shaped like a dragon sailing a sea of beer"
+    },
+    {
+        q:"According to the Canadian Government, what range alcohol content should have?",
+        a:"1.1% - 8.6%",
+        btn1:"no more than 9%",
+        btn2:"0.5% - 6%",
+        btn3:"1.1% - 8.6%"
+    },
+    {
+        q:"What is the world's strongest beer?",
+        a:"Snake Venom",
+        btn1:"Snake Venom",
+        btn2:"Tarantula Venom",
+        btn3:"Komodo Dragon Spit"
+    },
+    {
+        q:"What is the ABV of the world's strongest beer?",
+        a:"67.5%",
+        btn1:"32%",
+        btn2:"40%",
+        btn3:"67.5%"
+    },
+    {
+        q:"What city expereeinced a flood of over 1.4 million liters of beer?",
+        a:"London",
+        btn1:"Berlin",
+        btn2:"London",
+        btn3:"Dublin"
+    },
+    {
+        q:"What animal likes beer the most?",
+        a:"Slugs",
+        btn1:"Slugs",
+        btn2:"Donkeys",
+        btn3:"Flies"
+    },
+    {
+        q:"How many gallons does a Keg hold?",
+        a:"15.5 gallons",
+        btn1:"17 gallons",
+        btn2:"15.5 gallons",
+        btn3:"10 gallons"
+    },
+    {
+        q:"What does yeast convert into in beer?",
+        a:"Ethanol and CO2",
+        btn1:"Ethanol and CO2",
+        btn2:"Foam and color",
+        btn3:"Bitterness and texture"
+    },
+    {
+        q:"What is the official scientific name of beer and beer-making?",
+        a:"Zythology",
+        btn1:"Zythology",
+        btn2:"Cisserone",
+        btn3:"Brewology"
+    },
+    {
+        q:"Why do bubbles tend to go up the center of the glass?",
+        a:"The friction is the least",
+        btn1:"What are you talking about?",
+        btn2:"The friction is the least",
+        btn3:"Because of the shape of the glass"
+    },
+    {
+        q:"How old is the oldest written beer recipe?",
+        a:"5000 years",
+        btn1:"1000 years",
+        btn2:"2500 years",
+        btn3:"5000 years"
+    },
+    {
+        q:"When was the first DUI recorded?",
+        a:"2000 bc",
+        btn1:"500 bc",
+        btn2:"200",
+        btn3:"2000 bc"
+    },
+    {
+        q:"How were pale ales invented?",
+        a:"The English added Hops to keep the beer from going bad.",
+        btn1:"This is a new fad created by craft breweries",
+        btn2:"The English added Hops to keep the beer from going bad.",
+        btn3:"The English added Hops to see what would happen."
+    },
+    {
+        q:"How expensive is the most expensive beer?",
+        a:"$1000",
+        btn1:"$100",
+        btn2:"$1000",
+        btn3:"$2000"
+    },
+    {
+        q:"What was the secret Inca ingredient?",
+        a:"Spit",
+        btn1:"Sweet Potato",
+        btn2:"Spit",
+        btn3:"Blue Corn"
+    },
+    {
+        q:"In what year were 6 packs created?",
+        a:"1935",
+        btn1:"1870",
+        btn2:"1920",
+        btn3:"1935"
+    }
+]
